dc1cf7b2e8169a03e618e5019162fdc5fcf753d2
Ayed <ayedf4397@gmail.com>
2025-03-18 00:29:16 +0100

    commit

--- Parser.c
+++ Parser.c
@@ -1,48 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdint.h>
 
-/*****************************************
- *         Parsing du header             
- * ***************************************/ 
+typedef enum {
+    MOVE=0,
+    LOADK=1,
+    LOADBOOL=2,
+    LOADNIL=3,
+    GETUPVAL=4,
+    GETGLOBAL=5,
+    GETTABLE=6,
+    SETGLOBAL=7,
+    SETUPVAL=8,
+    SETTABLE=9,
+    NEWTABLE=10,
+    SELF=11,
+    ADD=12,
+    SUB=13,
+    MUL=14,
+    DIV=15,
+    MOD=16,
+    POW=17,
+    UNM=18,
+    NOT=19,
+    LEN=20,
+    CONCAT=21,
+    JMP=22,
+    EQ=23,
+    LT=24,
+    LE=25,
+    TEST=26,
+    TESTSET=27,
+    CALL=28,
+    TAILCALL=29,
+    RETURN=30,
+    FORLOOP=31,
+    FORPREP=32,
+    TFORLOOP=33,
+    SETLIST=34,
+    CLOSE=35,
+    CLOSURE=36,
+    VARARG=37,
+}opcode;
 
+typedef enum {
+    VARARG_NEEDSARG=4,
+    VARARG_ISVARARG=2,
+    VARARG_HASARG=1,    
+}is_vararg_flag;
 /*****************************************
  *         Lire le fichier            
  * ***************************************/ 
 
-unsigned char* charger_fichier(const char* filepath) {
-    unsigned char* tab = (unsigned char*)malloc(12 * sizeof(unsigned char));
 
-    if (tab == NULL) {
-        perror("Erreur allocation de mémoire");
-        exit(0);
-    }
-
-    FILE* fichier_out = fopen(filepath, "rb");
-
-    if (fichier_out == NULL) {
-        perror("Erreur ouverture du fichier");
-        fclose(fichier_out);
-        exit(0);
-    }
-
-    size_t res = fread(tab, sizeof(unsigned char), 12, fichier_out);
-
-    if (res != 12) {
-        perror("Erreur lors du chargement dans le tableau");
-        fclose(fichier_out);
-        exit(0);
-    }
-
-    fclose(fichier_out);
-    return tab;
-}
 
 /*****************************************
- *         Lire le header           
+ *         Parsing du header           
  * ***************************************/ 
 
-struct Lua_header {
+typedef struct Lua_header {
 
     unsigned char header_signature[4]; //signature de Lua sur 4 octets
     unsigned int header_sig;         // les 4 octets concaténé pour avoir la signature en entier
@@ -56,10 +73,10 @@ struct Lua_header {
     unsigned char integral_flag;
 
     bool fit_for_consumption;
-};
+}Lua_header;
 
 // Passer la structure par référence pour modifier directement les valeurs
-void parsing_header(unsigned char* tab, struct Lua_header* header) {
+void parsing_header(unsigned char* tab, Lua_header* header) {
 
     header->fit_for_consumption= true;//initiaiser à true
     // Lecture de la signature
@@ -104,3 +121,65 @@ void parsing_header(unsigned char* tab, struct Lua_header* header) {
     printf("integral_flag: %u\n", tab[11]);
 }
 
+
+
+/*****************************************
+ *     Parsing des Function blocks      
+ * ***************************************/ 
+
+
+typedef struct String{    
+    long long unsigned int data_size;//pour etre sur qu'il soit capable de stocker SIZE_t donné par header
+    char *data;
+}String;
+
+
+ 
+
+typedef struct List_instr{
+    int size;
+
+}List_instr;
+
+
+typedef struct Lua_Fonction_block{
+    String source_name;
+    void *line_difined;              //void car taille inconnu pour le moment, donné par le header
+    void *last_line_defined;
+    long long unsigned int number_of_upvalues;
+    long long unsigned int number_of_parameters;
+    long long unsigned int vararg_flag;
+    long long unsigned int max_stack_size;
+}Lua_Fonction_block;
+
+typedef struct Lua_bytcode
+{
+    Lua_header Lua_header;
+    Lua_Fonction_block Lua_Fonction_block;
+    
+}Lua_bytcode;
+
+
+// Lire le tableau à partir de l'indice 12 car le hearder est de 12 octets.
+ void parsing_function_block(unsigned char* tab, Lua_bytcode* Lua_bytcode){
+
+   char hexStr[Lua_bytcode->Lua_header.size_of_size_t];
+
+   for(int i=12, j=0; i< 12+Lua_bytcode->Lua_header.size_of_size_t; i++){
+    printf("HELLO %llu\n",  tab[i]);
+            hexStr[j++]= tab[i];
+            printf("ffHELLO %llu\n",  tab[i]);
+
+     }
+
+   uint64_t hexvalue= strtoll(hexStr, NULL, 16);
+
+   Lua_bytcode->Lua_Fonction_block.source_name.data_size=hexvalue;
+
+
+   printf("Lua_bytcode->Lua_Fonction_block.source_name.data_size: %llu\n",  Lua_bytcode->Lua_Fonction_block.source_name.data_size);
+   printf("Lua_bytcode->Lua_Fonction_block.source_name.data_size: %s\n",  Lua_bytcode->Lua_Fonction_block.source_name.data_size);
+   printf("hexvalue: %llu\n",  hexvalue);
+   printf("hexvalue: %s\n",  hexvalue);
+
+ }
